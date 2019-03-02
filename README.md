e25d88174c11559d87fcecf9729fe92f7d0d607f
wuyw <w2499115366@126.com>
2019-03-02 17:16:24 +0800

    README.md

--- README.md
+++ README.md
@@ -1,9 +1,41 @@
-### regCode
+## regCode
 > canvas实现的验证码
-### 参数
+## 用法
+``` javascript
+    /* 
+        下载的js文件
+        <script src="index.js"> </script>
+    */
+    // or
+    /* 
+        npm install vchat-regcode --save
+        import Canvas from 'vchat-regcode';
+    */
+    
+    let regcode = new Canvas(document.querySelector('#regcode'),{
+        fontSize: 20,
+        lineNum: 2,
+        dotNum: 10
+    }); // 实例化
+    
+    regcode.draw((r) => { // 绘制验证码，在回调函数中可以获取图片显示验证码文字，从而判断与用户输入的是否一致
+        console.log(r);
+    });
+```
+## 参数
 
 | 参数 | 描述 | 类型 | 默认值 |
 | :-: | :-: | :-: | :-: | 
 | lineWidth | 线宽 | Number | 0.5 |
 | lineNum | 线的条数 | Number | 5 |
 | dotNum  | 点的数量 | Number | 20 |
+| dotR  | 点的半径 | Number | 1 |
+| foregroundColor  | 前景色区间 | Array | [10, 80] |
+| backgroundColor  | 背景色区间 | Array | [150, 250] |
+| fontSize  | 字体大小 | Number | 28 |
+| fontFamily  | 字体类型 | String | 'Georgia' |
+| content  | 验证码因子 | String | 'acdefhijkmnpwxyABCDEFGHJKMNPQWXY12345789' |
+| fontStyle | 字体绘制类型 | 'fill'/'stroke' (填充or无填充) |
+| len  | 验证码长度 | Number | 4 |
+
+> 颜色区间数组是指rgb颜色的强度值(0-255, 从暗—>亮)
--- index.js
+++ index.js
@@ -2,15 +2,15 @@ class Canvas {
     constructor(canvas, config = {}) {
         this.canvas = canvas;
         this.paint = this.canvas.getContext('2d');
-        this.lineWidth = config.lineWidth || 0.5;
-        this.lineNum = config.lineNum || 5;
-        this.dotNum = config.dotNum || 20;
-        this.dotR = config.dotR || 1;
-        this.foregroundColor = config.foregroundColor || [10, 80];
-        this.backgroundColor = config.backgroundColor || [150, 250];
-        this.fontSize = config.fontSize || 28;
-        this.fontFamily = config.fontFamily || 'Georgia';
-        this.fontStyle = config.fontStyle || 'fill'; // stroke
+        this.lineWidth = config.lineWidth || 0.5; // 线条宽度
+        this.lineNum = config.lineNum || 5; // 线条数量
+        this.dotNum = config.dotNum || 20; // 点的数量
+        this.dotR = config.dotR || 1; // 点的半径
+        this.foregroundColor = config.foregroundColor || [10, 80]; // 前景色区间
+        this.backgroundColor = config.backgroundColor || [150, 250]; // 背景色区间
+        this.fontSize = config.fontSize || 28; // 字体大小
+        this.fontFamily = config.fontFamily || 'Georgia'; // 字体类型
+        this.fontStyle = config.fontStyle || 'fill'; // 字体绘制方法，fill/stroke
         this.content = config.content || 'acdefhijkmnpwxyABCDEFGHJKMNPQWXY12345789'; // 验证码的因子
         this.len = config.len || 4; // 验证码长度
         this.callback = function () {};
@@ -19,7 +19,7 @@ class Canvas {
         }
     }
 
-    getColor(arr) {
+    getColor(arr) { // 随机获取颜色
         let a, b, color = [];
         if (arr[0] > arr [1]) {
             a = arr [1];
@@ -34,11 +34,11 @@ class Canvas {
         return color;
     }
 
-    getRand(a, b) {
+    getRand(a, b) { // 获取某个区间的随机数
         return Math.floor(Math.random() * (b - a) + a);
     }
 
-    getText() {
+    getText() { // 随机获取验证码
         let len = this.content.length, str = '';
         for (let i = 0; i < this.len; i++) {
             str += this.content[this.getRand(0, len)];
@@ -46,7 +46,7 @@ class Canvas {
         return str;
     }
 
-    line() {
+    line() { // 绘制线条
         for (let i = 0; i < this.lineNum; i++) {
             let x = this.getRand(0, this.canvas.width), y = this.getRand(0, this.canvas.height),
                 endx = this.getRand(0, this.canvas.width), endy = this.getRand(0, this.canvas.width);
@@ -60,7 +60,7 @@ class Canvas {
         }
     }
 
-    arc() {
+    arc() { // 绘制圆点
         for (let i = 0; i < this.dotNum; i++) {
             let x = this.getRand(0, this.canvas.width), y = this.getRand(0, this.canvas.height);
             this.paint.beginPath();
@@ -71,7 +71,7 @@ class Canvas {
         }
     }
 
-    font() {
+    font() { // 绘制文字
         let str = this.getText();
         this.callback(str);
         this.paint.font = `${this.fontSize}px ${this.fontFamily}`;
@@ -90,7 +90,7 @@ class Canvas {
         }
     }
 
-    draw(callback) {
+    draw(callback) { // 画图
         this.callback = callback;
         this.paint.fillStyle = `rgba(${this.getColor(this.backgroundColor)[0]}, ${this.getColor(this.backgroundColor)[1]}, ${this.getColor(this.backgroundColor)[2]}, 0.8)`;
         this.paint.fillRect(0, 0, this.canvas.width, this.canvas.height);
@@ -99,11 +99,11 @@ class Canvas {
         this.font();
     }
 
-    clear() {
+    clear() { // 清除画布
         this.paint.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 
-    drawAgain() {
+    drawAgain() { // 更新画布
         this.clear();
         this.draw(this.callback);
     }
